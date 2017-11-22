838d168250a5e5a83ce70f4b15ab4df6dafbe482
wbarret1 <barrett.williamm@epa.gov>
2017-11-22 12:34:37 -0500

    First commit

---
+++ Reactors/Chemical.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reactors
+{
+    class Chemical
+    {
+        public Chemical()
+        {
+
+        }
+        public string Name { get; set; }
+
+
+        private string m_CasNo;
+        public string CasNo
+        {
+            get
+            {
+                return m_CasNo;
+            }
+            set
+            {
+                string[] numbers = value.Split('-');
+                if (numbers.Length != 3)
+                {
+                    throw new System.Exception("Incorrect format.");
+                }
+                m_CasNo = numbers[0] + "-" + numbers[1] + "-" + numbers[2];
+            }
+        }
+        public double MolecularWeight { get; set; }
+        public double heatOfFormation { get; set; }
+        public double entropyOfFormation { get; set; }
+
+    }
+}
---
+++ Reactors/checicals.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reactors
+{
+    class checicals
+    {
+    }
+
+
+    public class Rootobject
+    {
+        public Class1[] Property1 { get; set; }
+    }
+
+    public class Class1
+    {
+        public int id { get; set; }
+        public string casNo { get; set; }
+        public string accessionNo { get; set; }
+        public string pmnNo { get; set; }
+        public string epaId { get; set; }
+        public Synonym[] synonyms { get; set; }
+    }
+
+    public class Synonym
+    {
+        public int id { get; set; }
+        public string chemicalName { get; set; }
+        public bool isIupac { get; set; }
+        public bool isTscaInv { get; set; }
+        public bool isRegistry { get; set; }
+        public bool isSystematic { get; set; }
+        public bool isUnregistered { get; set; }
+        public bool isWorkPlan { get; set; }
+        public bool isTsca12B { get; set; }
+        public int sortOrder { get; set; }
+    }
+
+}
